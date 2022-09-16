a547a26d48ba0deecfeb4d37115fc26dc5557411
Haonan Lu <hnlu546@gmail.com>
2022-09-15 21:07:20 -0400

    format

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world from Haonan Lu!
-        </p>
-        <a className="App-navi" href="labs/a2/index.html">Assignment 2 Labs</a>
-        <a className="App-navi" href="tuiter/navigation.html">Tuiter</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Hello world from Haonan Lu!
+          </p>
+          <a className="App-navi" href="labs/a2/index.html">Assignment 2
+            Labs</a>
+          <a className="App-navi" href="tuiter/navigation.html">Tuiter</a>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
